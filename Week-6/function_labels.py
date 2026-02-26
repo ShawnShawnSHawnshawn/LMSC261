b29b7ee5fdab620db847c77bb4f789922edca63c
ShawnShawnSHawnshawn <spark43@berklee.edu>
2026-02-26 15:54:59 -0500

    add Week6 files

---
+++ Week-6/function_labels.py
@@ -0,0 +1,83 @@
+# int() function
+
+# Example of calling the int() function
+age = int("30")
+
+# Name: int
+# Arguments: Defining the age
+# Side Effects: none
+# Return Value: 30
+
+____________________________________________________________________________________________________________________
+
+# str() function
+
+# Example of calling the str() function
+name = str("Shawn")
+
+# Name: str
+# Arguments: Defining the name
+# Side Effects: none
+# Return Value: Shawn
+
+____________________________________________________________________________________________________________________
+
+# bool() function
+
+# Example of calling the bool() function
+is_true = bool((5 > 3) and (10 == 10))
+
+# Name: bool
+# Arguments: Defining True or False
+# Side Effects: none
+# Return Value: true
+
+____________________________________________________________________________________________________________________
+
+# float() function
+
+# Example of calling the float() function
+volume = float("0.9")
+
+# Name: float
+# Arguments: Defining the float
+# Side Effects: none
+# Return Value: 0.9
+
+____________________________________________________________________________________________________________________
+
+# len() function
+
+# Example of calling the len() function
+letters = len("Coding")
+
+# Name: len
+# Arguments: Defining the length of the letter
+# Side Effects: None
+# Return Value: 6
+
+____________________________________________________________________________________________________________________
+
+
+# pow() function
+
+# Example of calling the pow() function
+power_result = pow(3, 2)
+
+# Name: pow
+# Arguments: x, y => x to the power of y
+# Side Effects: None
+# Return Value: 9
+
+____________________________________________________________________________________________________________________
+
+
+# abs() function
+
+# Example of calling the abs() function
+distance = abs(-25)
+
+# Name: abs
+# Arguments: x - a number
+# Side Effects: None
+# Return Value: 25
---
+++ calculation.py
@@ -0,0 +1,17 @@
+print(abs(-25))
+
+print(len("Coding is fun"))
+
+print(pow(2, 4))
+
+print(type(15.5))
+
+print(int(12.9))
+
+print(float(len(str(abs(-100)))))
+
+print(bool(pow(int(2.9), 0)))
+
+print(type(str(type(42))))
+
+print(len(str(bool(0))))
